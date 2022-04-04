6a59c33b82ee5b05c2b2bed70993cadf76ec4b47
kyoulho <kyoulho@gmail.com>
2022-04-04 11:55:15 +0900

    Favorite

--- src/main/java/com/ezenac/thunder_market/member/domain/Favorite.java
+++ src/main/java/com/ezenac/thunder_market/member/domain/Favorite.java
@@ -1,10 +1,26 @@
 package com.ezenac.thunder_market.member.domain;
 
 import com.ezenac.thunder_market.product.domain.Product;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class Favorite {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long favoriteId;
 
+    @ManyToOne(fetch = FetchType.LAZY)
     private Product product;
 
+    @ManyToOne(fetch = FetchType.LAZY)
     private Member member;
 }
