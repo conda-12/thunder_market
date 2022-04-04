4e00285a0731b48e32825effb5ffdb642fa77829
kyoulho <kyoulho@gmail.com>
2022-04-04 13:02:57 +0900

    homecontroller 수정

--- src/main/java/com/ezenac/thunder_market/HomeController.java
+++ src/main/java/com/ezenac/thunder_market/HomeController.java
@@ -1,26 +1,14 @@
 package com.ezenac.thunder_market;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-
+@Controller
 public class HomeController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String home(Locale locale, Model model) {
-
-        Date date = new Date();
-        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-
-        String formattedDate = dateFormat.format(date);
-
-        model.addAttribute("serverTime", formattedDate );
+    @GetMapping("/")
+    public String home(Model model) {
 
         return "index";
     }
