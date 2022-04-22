ac80e2f568b58c531c5993ad0db4a216f2097e6c
conda-12 <83756206+conda-12@users.noreply.github.com>
2022-04-22 14:39:14 +0900

    Update SecurityConfig.java
    충동 해결

--- src/main/java/com/ezenac/thunder_market/config/SecurityConfig.java
+++ src/main/java/com/ezenac/thunder_market/config/SecurityConfig.java
@@ -1,24 +1,40 @@
-package com.ezenac.thunder_market.config;
+package com.ezenac.thunder_market.security.config;
 
-
-import com.ezenac.thunder_market.config.auth.PrincipalDetailsService;
-import com.ezenac.thunder_market.config.oauth.PrincipalOauth2MemberService;
+import com.ezenac.thunder_market.security.auth.PrincipalDetailsService;
+import com.ezenac.thunder_market.security.factory.UrlResourcesMapFactoryBean;
+import com.ezenac.thunder_market.security.filter.PermitAllFilter;
+import com.ezenac.thunder_market.security.metadatasource.UrlFilterInvocationSecurityMetadataSource;
+import com.ezenac.thunder_market.security.oauth.PrincipalOauth2MemberService;
+import com.ezenac.thunder_market.security.service.SecurityResourceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.access.vote.AffirmativeBased;
+import org.springframework.security.access.vote.RoleHierarchyVoter;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true) // 어노테이션으로 접근제한 설정
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
     private final PrincipalOauth2MemberService principalOauth2MemberService;
     private final PrincipalDetailsService principalDetailsService;
-
+    private final SecurityResourceService securityResourceService;
+    private final String[] permitAll = {"/"};
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -26,16 +42,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
 
         http.authorizeRequests()
-                .antMatchers("/member/auth/signin", "/member/auth/register").permitAll()
-                .antMatchers("/member/**").access("hasRole('ROLE_MEMBER') or hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
-                .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
-                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
                 .loginPage("/member/auth/signin")
                 .usernameParameter("memberId")
-                .loginProcessingUrl("/member/auth/signin").defaultSuccessUrl("/")
+                .loginProcessingUrl("/member/auth/signin")
+                .defaultSuccessUrl("/")
                 .and()
                 .rememberMe()
                 .key("@thunder_market_remember-me_cookie")
@@ -49,9 +62,52 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .invalidateHttpSession(true)
                 .deleteCookies("remember-me", "JSESSIONID")
                 .and()
+                .addFilterBefore(customFilterSecurityInterceptor(), FilterSecurityInterceptor.class)
                 .oauth2Login()
                 .loginPage("/member/auth/signin")
                 .userInfoEndpoint()
                 .userService(principalOauth2MemberService);
+    }
+
+
+    @Bean
+    public PermitAllFilter customFilterSecurityInterceptor() throws Exception {
+        PermitAllFilter permitAllFilter = new PermitAllFilter(permitAll);
+        permitAllFilter.setSecurityMetadataSource(urlFilterInvocationSecurityMetadataSource());
+        permitAllFilter.setAccessDecisionManager(affirmativeBased());
+        permitAllFilter.setAuthenticationManager(authenticationManagerBean());
+
+        return permitAllFilter;
+    }
+
+    private AccessDecisionManager affirmativeBased() {
+        return new AffirmativeBased(getAccessDecisionVoters());
+    }
+
+    private List<AccessDecisionVoter<?>> getAccessDecisionVoters() {
+
+        List<AccessDecisionVoter<?>> accessDecisionVoters = new ArrayList<>();
+        accessDecisionVoters.add(roleVoter());
+
+        return accessDecisionVoters;
+    }
+
+    private AccessDecisionVoter<?> roleVoter() {
+
+        return new RoleHierarchyVoter(roleHierarchy());
+    }
+
+    @Bean
+    public RoleHierarchyImpl roleHierarchy() {
+        return new RoleHierarchyImpl();
+    }
+
+    @Bean
+    public UrlFilterInvocationSecurityMetadataSource urlFilterInvocationSecurityMetadataSource() throws Exception {
+        return new UrlFilterInvocationSecurityMetadataSource(urlResourcesMapFactoryBean().getObject(), securityResourceService);
+    }
+
+    private UrlResourcesMapFactoryBean urlResourcesMapFactoryBean() {
+        return new UrlResourcesMapFactoryBean(securityResourceService);
     }
 }
