f97768801aed9f647c9be1ab6754879f39514e5d
kyoulho <kyoulho@gmail.com>
2022-04-25 10:20:10 +0900

    refactoring

--- src/main/java/com/ezenac/thunder_market/repository/ProductRepository.java
+++ src/main/java/com/ezenac/thunder_market/repository/ProductRepository.java
@@ -6,17 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
-
-    @Query("SELECT p, COUNT (f)" +
-            " FROM Product p" +
-            " LEFT JOIN Favorite f ON f.product = p" +
-            " WHERE p.state = 'SELLING'" +
-            " GROUP BY p")
-    Page<Object[]> getList(Pageable pageable);
 
     @Query("SELECT p" +
             " FROM Product p" +
@@ -27,11 +21,11 @@ public interface ProductRepository extends JpaRepository<Product, Long>, Product
             " FROM Product p" +
             " LEFT JOIN Favorite f ON f.product = p" +
             " WHERE p.state = 'SELLING' AND p.productId = :productId")
-    Optional<Object> readWithFavorite(Long productId);
+    Optional<Object> readWithFavorite(@Param("productId")Long productId);
 
     @Modifying
     @Query("UPDATE Product p" +
             " set p.hit = p.hit + 1" +
             " WHERE p.productId = :productId")
-    void updateHit(Long productId);
+    void updateHit(@Param("productId") Long productId);
 }
---
+++ src/test/java/com/ezenac/thunder_market/service/FavoriteServiceTest.java
@@ -0,0 +1,9 @@
+package com.ezenac.thunder_market.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FavoriteServiceTest {
+
+}
